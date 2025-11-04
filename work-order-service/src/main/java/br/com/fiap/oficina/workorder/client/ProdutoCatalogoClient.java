7e933086e2c3aec33470ff0db4fc2be8d13950d0
GrecoMilani <greco_lima@hotmail.com>
2025-11-03 22:21:08 -0300

    feat: add contextId to FeignClient annotations for improved client identification

--- work-order-service/src/main/java/br/com/fiap/oficina/workorder/client/ProdutoCatalogoClient.java
+++ work-order-service/src/main/java/br/com/fiap/oficina/workorder/client/ProdutoCatalogoClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "catalog-service")
+@FeignClient(name = "catalog-service", contextId = "produto-catalogo-client")
 public interface ProdutoCatalogoClient {
 
     @GetMapping("/api/catalogo-produtos/{id}")
