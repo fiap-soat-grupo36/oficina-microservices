436f6336488f38fed91c24fad57a0a55457de1d5
GrecoMilani <greco_lima@hotmail.com>
2026-01-07 19:23:58 -0300

    Revert "fix: comment Datadog resources to avoid conflicts with infra-kubernetes"
    This reverts commit 109c97cd8e573ca6fa5fb75dca486d54c50d928e.

--- infra/datadog.tf
+++ infra/datadog.tf
@@ -1,38 +1,42 @@
-# Datadog Agent gerenciado pelo infra-kubernetes via EKS Addon
-# Este código foi comentado para evitar conflitos e dependências circulares
-# O Datadog Operator é instalado via EKS addon no projeto infra-kubernetes
+# Namespace para Datadog Agent
+resource "kubernetes_namespace" "datadog" {
+  metadata {
+    name = "datadog-agent"
+  }
+}
 
-# # Namespace para Datadog Agent
-# resource "kubernetes_namespace" "datadog" {
-#   metadata {
-#     name = "datadog-agent"
-#   }
-# }
-#
-# resource "kubernetes_secret" "datadog" {
-#   metadata {
-#     name      = "datadog"
-#     namespace = kubernetes_namespace.datadog.metadata[0].name
-#   }
-#
-#   data = {
-#     "api-key" = var.datadog_api_key
-#     "app-key" = var.datadog_app_key
-#   }
-#
-#   depends_on = [kubernetes_namespace.datadog]
-# }
-#
-# data "kubectl_path_documents" "dd_agent" {
-#   pattern = "../k8s/datadog/datadog-agent.yaml"
-# }
-#
-# resource "kubectl_manifest" "dd_agent_manifest" {
-#   for_each  = data.kubectl_path_documents.dd_agent.manifests
-#   yaml_body = each.value
-#
-#   depends_on = [
-#     kubernetes_namespace.datadog,
-#     kubernetes_secret.datadog
-#   ]
-# }
+# Secret com credenciais do Datadog
+resource "kubernetes_secret" "datadog" {
+  metadata {
+    name      = "datadog"
+    namespace = kubernetes_namespace.datadog.metadata[0].name
+  }
+
+  data = {
+    "api-key" = var.datadog_api_key
+    "app-key" = var.datadog_app_key
+  }
+
+  depends_on = [kubernetes_namespace.datadog]
+}
+
+# DatadogAgent CRD - só aplica se o Datadog Operator (do infra-kubernetes) já instalou o CRD
+data "kubectl_path_documents" "dd_agent" {
+  pattern = "../k8s/datadog/datadog-agent.yaml"
+}
+
+resource "kubectl_manifest" "dd_agent_manifest" {
+  for_each  = data.kubectl_path_documents.dd_agent.manifests
+  yaml_body = each.value
+
+  # Usar server-side apply para evitar conflitos
+  server_side_apply = true
+
+  # Aguardar o recurso ser criado completamente
+  wait = true
+
+  depends_on = [
+    kubernetes_namespace.datadog,
+    kubernetes_secret.datadog
+  ]
+}
